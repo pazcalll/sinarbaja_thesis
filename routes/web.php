a7608a0d412f8b9b4a9d80ef28fd18aaa29eb76d
pazcalll <yazeedarifin@gmail.com>
2022-07-14 18:32:27 +0700

    cleaning

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,22 @@
 <?php
 
-use App\HargaProdukGroup;
+// use App\HargaProdukGroup;
 use App\Http\Controllers\CatalogueController;
-use App\Http\Controllers\MenuController;
-use App\Http\Controllers\DriverController;
-use App\Http\Controllers\HargaProdukGroupController;
-use App\Http\Controllers\HargaProdukUserController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\TrackingController;
-use App\Http\Controllers\ProductController;
+// use App\Http\Controllers\MenuController;
+// use App\Http\Controllers\DriverController;
+// use App\Http\Controllers\HargaProdukGroupController;
+// use App\Http\Controllers\HargaProdukUserController;
+// use App\Http\Controllers\OrderController;
+// use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ThesisAdminOrderController;
 use App\Http\Controllers\ThesisCustomerController;
 use App\Http\Controllers\ThesisGroupUserController;
 use App\Http\Controllers\ThesisItemController;
 use App\Http\Controllers\ThesisUserController;
-use App\Http\Controllers\UserController;
+// use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -38,7 +37,7 @@ Route::get('/filter', [CatalogueController::class, 'filter']);
 Route::get('/index-tabel', [CatalogueController::class, 'tabel']);
 Route::get('/not-found', [CatalogueController::class, 'notFound']);
 Route::get('/product/{id}', [CatalogueController::class, 'show']);
-Route::get('/tracking', [TrackingController::class, 'client']);
+// Route::get('/tracking', [TrackingController::class, 'client']);
 
 Route::get('/detail/product/{barang_alias}', [CatalogueController::class, 'detailTable']);
 Route::post('/detail/produk','CatalogueController@get_detailBarang')->name('get_detailBarang');
@@ -47,7 +46,7 @@ Route::post('delete_cart','cartController@delete_cart')->name('delete_cart');
 Route::post('/cart','cartController@cartProcess')->name('addCart');
 Route::post('cartData','cartController@cartData')->name('cartData');
 Route::post('json_cartAll','cartController@json_cartAll')->name('json_cartAll');
-Route::get('count_harganull','notifHandlerController@count_harganull')->name('count_harganull');
+// Route::get('count_harganull','notifHandlerController@count_harganull')->name('count_harganull');
 
 Route::post('/login', 'ThesisUserController@login')->name('loginUser');
 Route::post('/register', 'ThesisUserController@create')->name('registerUser');
@@ -59,57 +58,51 @@ Route::get('/user-cart-test', 'cartController@userCartTest');
 Route::prefix('data')->group(function () {
 
     Route::get('user', [UserController::class, 'authGetter'])->name('authGetter');
-    Route::post('/create/group', [UserController::class, 'createGroup'])->name('createGroup');
-    Route::post('/create/user', [UserController::class, 'createUser'])->name('createUser');
-    Route::get('/delete/group/{id}', [UserController::class, 'destroyGroup']);
-    Route::get('/delete/user/{id}', [UserController::class, 'destroyUser']);
-    Route::post('/edit/group', [UserController::class, 'editGroup'])->name('editGroup');
-    Route::post('/edit/user', [UserController::class, 'editUser'])->name('editUser');
+    // Route::post('/create/group', [UserController::class, 'createGroup'])->name('createGroup');
+    // Route::post('/create/user', [UserController::class, 'createUser'])->name('createUser');
+    // Route::get('/delete/group/{id}', [UserController::class, 'destroyGroup']);
+    // Route::get('/delete/user/{id}', [UserController::class, 'destroyUser']);
+    // Route::post('/edit/group', [UserController::class, 'editGroup'])->name('editGroup');
+    // Route::post('/edit/user', [UserController::class, 'editUser'])->name('editUser');
 
-    Route::get('user-group', [UserController::class, 'allGroup'])->name('allGroup');
-    Route::post('user', [HargaProdukGroupController::class, 'store'])->name('storeGroupHarga');
-    Route::post('user/harga/all', [HargaProdukUserController::class, 'store'])->name('storeUserHarga');
-    Route::post('user/harga/deleteAll', [HargaProdukUserController::class, 'deleteByGroup'])->name('deleteAllUserHarga');
-    Route::get('user/harga/byProduk/{produkId}', [HargaProdukUserController::class, 'usersWithPrices'])->name('usersWithPrices');
-    Route::get('user/harga/byProduk/groupPriceSelection/{productId}', [HargaProdukUserController::class, 'groupPriceSelection'])->name('groupPriceSelection');
-    Route::post('user/harga/byProduk/changeUserPrice', [HargaProdukUserController::class, 'changeUserPrice'])->name('changeUserPrice');
+    // Route::get('user-group', [UserController::class, 'allGroup'])->name('allGroup');
+    // Route::post('user', [HargaProdukGroupController::class, 'store'])->name('storeGroupHarga');
+    // Route::post('user/harga/all', [HargaProdukUserController::class, 'store'])->name('storeUserHarga');
+    // Route::post('user/harga/deleteAll', [HargaProdukUserController::class, 'deleteByGroup'])->name('deleteAllUserHarga');
+    // Route::get('user/harga/byProduk/{produkId}', [HargaProdukUserController::class, 'usersWithPrices'])->name('usersWithPrices');
+    // Route::get('user/harga/byProduk/groupPriceSelection/{productId}', [HargaProdukUserController::class, 'groupPriceSelection'])->name('groupPriceSelection');
+    // Route::post('user/harga/byProduk/changeUserPrice', [HargaProdukUserController::class, 'changeUserPrice'])->name('changeUserPrice');
 
     //setting harga user
-    Route::post('setting_harga/user/setHargaUser', 'HargaUserController@setHarga')->name('setHargaUser');
-    Route::get('setting_harga/user/hargaUser/{userId}/{groupId}', 'HargaUserController@hargaUser')->name('hargaUser');
-    Route::get('setting_harga/user/pilihHarga/{userId}/{groupId}', 'HargaUserController@pilihHarga')->name('pilihHarga');
-    Route::post('setting_harga/user/harga', 'HargaUserController@hargaCheckbox')->name('hargaCheckbox');
-    Route::post('setting_harga/user/hargaAll', 'HargaUserController@saveAll')->name('saveAllHargaUser');
+    // Route::post('setting_harga/user/setHargaUser', 'HargaUserController@setHarga')->name('setHargaUser');
+    // Route::get('setting_harga/user/hargaUser/{userId}/{groupId}', 'HargaUserController@hargaUser')->name('hargaUser');
+    // Route::get('setting_harga/user/pilihHarga/{userId}/{groupId}', 'HargaUserController@pilihHarga')->name('pilihHarga');
+    // Route::post('setting_harga/user/harga', 'HargaUserController@hargaCheckbox')->name('hargaCheckbox');
+    // Route::post('setting_harga/user/hargaAll', 'HargaUserController@saveAll')->name('saveAllHargaUser');
 
     Route::get('catalogue/products/detail/{barang_alias}', [CatalogueController::class, 'tableDetail']);
     Route::get('catalogue/products', [CatalogueController::class, 'catalogue']);
     Route::get('catalogue/harga-group', [CatalogueController::class, 'hargaGroup'])->name('hargaGroup');
 
-    Route::get('/order/histori-return', [OrderController::class, 'returnHistori']);
-    Route::get('/order/{id_user}/prosses', [OrderController::class, 'proses']);
-    Route::get('/order/{id_user}/pesanan-proses', [OrderController::class, 'pesananProses']);
-    Route::get('/order/{id_user}/pesanan-kirim', [OrderController::class, 'pesananKirim']);
-    Route::get('/order/{id_user}/tertunda', [OrderController::class, 'tertunda']);
-    Route::get('/order/{id_user}/return', [OrderController::class, 'return']);
-    Route::get('/order/{id_user}/pesanan-selesai', [OrderController::class, 'pesananSelesai']);
-    Route::get('/order/pesanan-diterima/{id_tagihan}', [OrderController::class, 'pesananDiterima']);
-    Route::get('/order/riwayat', [OrderController::class, 'dataRiwayat']);
-    Route::get('/order/pesanan-selesai', [OrderController::class, 'dataSelesai']);
-    Route::post('/order/return', [OrderController::class, 'storeReturn']);
-    Route::post('/order/pickup', [OrderController::class, 'storePickup'])->name('storePickup');
-    Route::post('/order/arrive', [OrderController::class, 'storeArrive'])->name('storeArrive');
-    Route::post('/order/confirm', [OrderController::class, 'storeConfirm'])->name('storeConfirm');
+    // Route::get('/order/histori-return', [OrderController::class, 'returnHistori']);
+    // Route::get('/order/{id_user}/prosses', [OrderController::class, 'proses']);
+    // Route::get('/order/{id_user}/pesanan-proses', [OrderController::class, 'pesananProses']);
+    // Route::get('/order/{id_user}/pesanan-kirim', [OrderController::class, 'pesananKirim']);
+    // Route::get('/order/{id_user}/tertunda', [OrderController::class, 'tertunda']);
+    // Route::get('/order/{id_user}/return', [OrderController::class, 'return']);
+    // Route::get('/order/{id_user}/pesanan-selesai', [OrderController::class, 'pesananSelesai']);
+    // Route::get('/order/pesanan-diterima/{id_tagihan}', [OrderController::class, 'pesananDiterima']);
+    // Route::get('/order/riwayat', [OrderController::class, 'dataRiwayat']);
+    // Route::get('/order/pesanan-selesai', [OrderController::class, 'dataSelesai']);
+    // Route::post('/order/return', [OrderController::class, 'storeReturn']);
+    // Route::post('/order/pickup', [OrderController::class, 'storePickup'])->name('storePickup');
+    // Route::post('/order/arrive', [OrderController::class, 'storeArrive'])->name('storeArrive');
+    // Route::post('/order/confirm', [OrderController::class, 'storeConfirm'])->name('storeConfirm');
 
     //menu
-    Route::post('/setting/menu/create', 'MenuController@create');
-    Route::get('/setting/menu/delete/{id}', 'MenuController@destroy');
-    Route::post('/setting/menu/edit', 'MenuController@edit')->name('editMenu');
-
-    //return
-    Route::get('/return/data', 'ReturnController@getData')->name('return_barang');
-
-    Route::get('/drivers', [DriverController::class, 'index']);
-    Route::get('/tracking', 'TrackingController@data');
+    // Route::post('/setting/menu/create', 'MenuController@create');
+    // Route::get('/setting/menu/delete/{id}', 'MenuController@destroy');
+    // Route::post('/setting/menu/edit', 'MenuController@edit')->name('editMenu');
 
     // thesis
     Route::get('/getter', 'ThesisController@getter');
@@ -187,10 +180,10 @@ Route::group(['middleware' => ['auth', 'customer']], function () {
     // Route::resource('/profile', 'ProfilController');
     // Route::post('/profile_update/{id?}', 'ProfilController@update')->name('profile_update');
 
-    Route::get('/profile_pending', 'ProfilController@pending');
-    Route::get('/profile_return', 'ProfilController@return');
+    // Route::get('/profile_pending', 'ProfilController@pending');
+    // Route::get('/profile_return', 'ProfilController@return');
 
     // purchase-order
     Route::post('/order/purchase-order', 'PurchaseOrderController@store')->name('storeOrder');
-    Route::post('/order/upload', 'TagihanController@upload');
+    // Route::post('/order/upload', 'TagihanController@upload');
 });
